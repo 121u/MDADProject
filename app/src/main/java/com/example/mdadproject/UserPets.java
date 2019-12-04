04d7e1d32790f290e460afc1eff9a746f6af5272
121u <jasontoddfanboi@gmail.com>
2019-12-04 15:44:20 +0800

    test merge

---
+++ app/src/main/java/com/example/mdadproject/UserPets.java
@@ -0,0 +1,14 @@
+package com.example.mdadproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_pets);
+    }
+}
