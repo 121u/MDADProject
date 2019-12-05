3e5170da55691a682efc4d7c00c7ca0109516557
121u <jasontoddfanboi@gmail.com>
2019-12-05 23:11:55 +0800

    nav drawer

--- app/src/main/java/com/example/mdadproject/UserPets.java
+++ app/src/main/java/com/example/mdadproject/UserPets.java
@@ -1,14 +1,77 @@
 package com.example.mdadproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
-public class UserPets extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class UserPets extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_pets);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AppointmentFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_appointment);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_appointment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AppointmentFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_pet:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PetsFragment()).commit();
+                break;
+            case R.id.nav_qr:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new QRCodeFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
